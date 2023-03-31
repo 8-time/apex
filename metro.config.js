4af86b675b406a8e5120352bbfbd484e13c16f53
w.siteee <w.siteee@gmail.com>
2023-04-01 00:08:54 +0300

    update to 41 sdk

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (async () => {
   const {
@@ -7,6 +7,7 @@ module.exports = (async () => {
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== 'svg'),
